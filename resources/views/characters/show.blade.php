c5466cc7a13f8ef1f82db2df9f6ae5962a30bc7d
custompro98 <mitchjoa@gmail.com>
2024-09-20 23:55:50 -0400

    fix: Character show one column on xs-sm screens

--- resources/views/characters/show.blade.php
+++ resources/views/characters/show.blade.php
@@ -10,7 +10,7 @@
                     class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900" />
             </form>
         </div>
-        <div class="grid grid-cols-2 gap-4 pt-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4 pt-4">
             <section>
                 <form action="{{ route('characters.update', $character->id) }}" method="POST">
                     @csrf
@@ -176,5 +176,6 @@
                 </div>
             </section>
         </div>
+    </div>
     </div>
 </x-app-layout>
